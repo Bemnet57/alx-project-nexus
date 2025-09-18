facb3bc7be52b32ef837333c19cb3f9dd1b9e665
Bemnet57 <bemnetasseged57@gmail.com>
2025-09-18 22:49:52 +0300

    add applications model and migrations

---
+++ job_platform/applications/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+# ENUM for status
+class StatusChoice(models.TextChoices):
+    PENDING = 'pending', 'Pending'
+    ACCEPTED = 'accepted', 'Accepted'
+    REJECTED = 'rejected', 'Rejected'
+
+class Application(models.Model):
+  job = models.ForeignKey('jobs.Job', on_delete=models.CASCADE, related_name='applications') #, 
+  applicant = models.ForeignKey(User, on_delete=models.CASCADE, related_name='job_applications') #, limit_choices_to={'role': UserRole.EMPLOYER}, help_text='The user who posted this job, must have the role of EMPLOYER.')
+  status = models.CharField(max_length=10, choices=StatusChoice.choices, default=StatusChoice.PENDING)
+  applied_at = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+        return f"Application for {self.job.title} by {self.applicant.get_full_name()}"
